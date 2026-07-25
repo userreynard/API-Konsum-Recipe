468c74b2634b4992e8acc514ca823fd7d69501c3
userreynard <abereynard19@gmail.com>
2026-07-26 00:28:33 +0700

    Fix merge conflict

--- resep-api/server.js
+++ resep-api/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const app = express();
-const PORT = 5000;
 
 app.use(express.static("public"));
 
@@ -17,6 +16,11 @@ app.get("/recipes", async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+// HANYA listen saat di localhost
+if (process.env.NODE_ENV !== "production") {
+    app.listen(5000, () => {
+        console.log("Server berjalan di http://localhost:5000");
+    });
+}
+
+module.exports = app;
